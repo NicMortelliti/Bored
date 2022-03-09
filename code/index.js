e5709ae21676e5a8a7064179c0e7cf2171d8043f
NicMortelliti <npmortelliti@gmail.com>
2022-03-08 16:31:33 -0800

    Started work on Activity Type drop down (HTML). Added necessary materialize code in JS for drop down. Started outlining JS layout

--- code/index.js
+++ code/index.js
@@ -1,4 +1,10 @@
+// TODO Define constant variable arrays here that will be used for the selection drop downs
+// typeOptions = ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]
+// participantOptions = [0, 1, 2, 3, "4+"] 
+// priceOptions = ["Free!", "I'm willing to pay"] <-- "willing to pay" will also include free options
+
 document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('select');
-  var instances = M.FormSelect.init(elems, options);
+  const elems = document.querySelectorAll('select');
+  const instance = M.FormSelect.init(elems);
+
 });
